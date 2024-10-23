2a378bd5a982039f866a5548e1c415ea66a0e19c
Rahulmeghna11 <sahebmm70@gmail.com>
2024-10-24 01:03:25 +0530

    Add files via upload

---
+++ sharpx.py
@@ -0,0 +1,17 @@
+# Don't use quotes( " and ' )
+#SCRIPT BY SharpX72
+  
+#Enter Your Bot Token here get it from @botfarher
+BOT_TOKEN=("8097510622:AAH9EyM8uolUP35eVULB4wixoBPaQobwrMU")
+
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("@RAHUL_DDOS_B")
+
+  #Enter your admin id here Get it from @missRose_bot by typing /info
+ADMIN_IDS=("1661744209")
+
+
+
+
+
+  
